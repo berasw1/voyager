03b3d757c8ea7ae41a177e5416b1147ce55f1b17
berasw1 <swatilekha.bera@novartis.com>
2016-08-28 17:50:43 +0530

    Update DataReceiverServlet.java

--- PAPilot/voyager/src/main/java/com/novartis/voyager/transport/DataReceiverServlet.java
+++ PAPilot/voyager/src/main/java/com/novartis/voyager/transport/DataReceiverServlet.java
@@ -21,7 +21,7 @@ import com.novartis.voyager.common.constants.CommonConstants;
 import com.novartis.voyager.util.CommonUtil;
 
 /**
- * Servlet implementation class DataReceiver for voyager. 
+ * Servlet implementation class DataReceiver for voyager. Test.
  */
 public class DataReceiverServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
